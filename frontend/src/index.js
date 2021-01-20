a26be1b34f6c81ebe130a0c81b72f76c5dd7a086
ruben <iamrub3n@gmail.com>
2021-01-20 16:46:56 +0545

    removed bootstrap import and done in index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDom from "react-dom"
-
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import "./style.css"
+//import 'antd/dist/antd.css'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDom.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.querySelector("#root")
 )
