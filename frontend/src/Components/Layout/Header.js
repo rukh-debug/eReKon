d0a3bb01f4802450626c68faeb617b1c34a4cde3
ruben <iamrub3n@gmail.com>
2021-01-20 16:49:31 +0545

    navigation complete

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
+import AuthNav from "../Auth/AuthNav"
 
 export default function Header() {
+
   return (
-    <div>
-      Header
-    </div>
+    <Container fluid>
+      <Navbar bg="dark" expand='lg' variant="dark" className="sticky-top pr-5 pl-5 m-6 navbar-dark border border-right-0 border-left-0 rounded my-nav">
+        <Navbar.Brand as={Link} to="/">eReKon</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <AuthNav />
+      </Navbar>
+      </Container>
   )
 }
+
 
