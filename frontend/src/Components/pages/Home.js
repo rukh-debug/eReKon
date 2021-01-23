5f081b384bff2e9ab383aaacc0d53ac1d6dc3fdb
ruben <iamrub3n@gmail.com>
2021-01-23 10:45:00 +0545

    loader added in home

--- frontend/src/Components/pages/Home.js
+++ frontend/src/Components/pages/Home.js
@@ -2,18 +2,30 @@ import React, { useContext } from 'react'
 import UserContext from '../../context/UserContext'
 import Scanner from "./Scanner/Scanner"
 import Landing from "./Landing"
+import { Container } from "react-bootstrap"
+import Loading from './Loading'
 
 export default function Home() {
 
   const { userData } = useContext(UserContext)
 
-  return (
-    <div>
-      {
-        userData.user
-          ? (<Scanner />)
-          : (<Landing />)
-    }
-    </div>
-  )
+  if (userData.isLoading) {
+    return (
+      <>
+      <Loading />
+      </>
+    )
+  }
+
+  else {
+    return (
+      <>
+        {
+          userData.user
+            ? (<Scanner />)
+            : (<Landing />)
+        }
+      </>
+    )
+  }
 }
