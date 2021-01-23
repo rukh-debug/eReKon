21aaf04d0f67c671b99f63b7adadcab533391b83
ruben <iamrub3n@gmail.com>
2021-01-23 10:45:13 +0545

    loading added and modified

--- frontend/src/Components/pages/Loading.js
+++ frontend/src/Components/pages/Loading.js
@@ -1,9 +1,14 @@
+import { faCircleNotch, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export default function Loading() {
   return (
-    <div>
-      Loading
-    </div>
+    <>
+      <Container>
+        <div className='go-center'><h1><FontAwesomeIcon className="fa-spin orange loading-container" icon={faCircleNotch} /> </h1></div>
+      </Container>
+    </>
   )
 }
