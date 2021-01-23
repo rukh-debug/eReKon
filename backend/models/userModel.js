4f88fc19d0adb83a3896a1f621fe24fe5b9706c4
ruben <iamrub3n@gmail.com>
2021-01-23 10:41:37 +0545

    scannedDataModel schema modified

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true, minlength: 5},
   displayName: {type: String},
+  uuid: {type: String},
 });
 
 let User;
