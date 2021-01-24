6fcf7fa176517154a823e14fd2f39aaa326d836d
ruben <iamrub3n@gmail.com>
2021-01-24 21:50:43 +0545

    removed progress route

--- backend/routes/reconRouter.js
+++ backend/routes/reconRouter.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 //const scanResponse = require('../sample.json')
 const app = require('../src/controller/app')
 const auth = require('../middleware/auth')
+const fs = require('fs');
 
 router.post('/fast', auth, async (req, res) => {
   try {
@@ -13,11 +14,11 @@ router.post('/fast', auth, async (req, res) => {
     // check if the user have ever saved his data or not...
     const findUserData = await ScannedData.findOne({ userRef: decoded.id })
     let folderNum;
-    if(findUserData){
+    if (findUserData) {
       let num = findUserData.scanData.length
       folderNum = num.toString()
     }
-    else{
+    else {
       folderNum = "0"
     }
     let scanResponse;
@@ -55,11 +56,12 @@ router.post('/fast', auth, async (req, res) => {
       // create new refrence for user if not created
       const userScanned = new ScannedData({
         userRef: decoded.id,
-        scanData: scanResponse
+        scanData: scanResponse,
+        forUrl: url
       })
       const userScannedData = await userScanned.save()
     }
-    res.status(200).json({scanResponse, folderNum})
+    res.status(200).json({ scanResponse, folderNum })
 
   }
   catch (e) {
@@ -71,13 +73,41 @@ router.post('/dash', auth, async (req, res) => {
   const { token } = req.body
   try {
     let decoded = jwt.verify(token, process.env.JWT_SECRETS)
-    console.log(decoded)
     const findUserData = await ScannedData.findOne({ userRef: decoded.id })
-    res.status(200).json({ data: findUserData })
+    res.status(200).json({ data: findUserData.scanData })
   }
   catch (e) {
     res.status(500).json({ error: e.message })
   }
 })
+
+//router.post('/progress', async (req, res) => {
+//  try {
+//    console.log('hello')
+//    let { token, uuid } = req.body;
+//    let decoded = jwt.verify(token, process.env.JWT_SECRETS)
+//    // check if the user have ever saved his data or not...
+//    const findUserData = await ScannedData.findOne({ userRef: decoded.id })
+//    let folderNum;
+//    if (findUserData) {
+//      let num = findUserData.scanData.length
+//      folderNum = num.toString()
+//    }
+//    else {
+//      folderNum = "0"
+//    }
+//
+//
+//    let raw = fs.readFileSync(`${__dirname}/../static/img/${uuid}/${folderNum}/progress.json`)
+//    let jsonData = JSON.parse(raw)
+//    console.log(jsonData)
+//    res.status(200).json(jsonData)
+//
+//
+//  }
+//  catch (e) {
+//    res.status(500).json({ msg: e.message })
+//  }
+//})
 
 module.exports = router;
