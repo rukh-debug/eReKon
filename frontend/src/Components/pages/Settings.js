e756bea498e3bec980f435038f9a02d630cec001
ruben <iamrub3n@gmail.com>
2021-01-29 22:33:37 +0545

    init settings

--- frontend/src/Components/pages/Settings.js
+++ frontend/src/Components/pages/Settings.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Row, Col, Card, Container } from 'react-bootstrap'
 
 export default function Profile() {
   return (
-    <div>
-      Settings
+    <Container>
+    <div className=' setting-container'>
+    <h3 className='text-muted'>User Settings</h3>
+
+      <Row>
+
+        <Col className='col-4'>
+          <Card bg="dark" className='card-container'>
+            Hello world
+          </Card>
+        </Col>
+
+        <Col className='col-8'>
+        <Card bg='dark' className='card-container'>
+          Hello
+        </Card>
+        </Col>
+
+      </Row>
     </div>
+    </Container>
   )
 }
