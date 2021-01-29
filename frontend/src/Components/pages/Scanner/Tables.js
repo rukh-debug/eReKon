40f275c83a204aedd57c56d3fd12638b25902030
ruben <iamrub3n@gmail.com>
2021-01-29 22:35:18 +0545

    added keys on elements

--- frontend/src/Components/pages/Scanner/Tables.js
+++ frontend/src/Components/pages/Scanner/Tables.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { OverlayTrigger, Tooltip, Table, Image, Container, Badge, Button } from "react-bootstrap";
 import classNames from 'classnames'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
