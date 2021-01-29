9b26529ec74dc7331a651683623a6ef2c5669375
ruben <iamrub3n@gmail.com>
2021-01-29 22:34:28 +0545

    added loading

--- frontend/src/Components/pages/Home.js
+++ frontend/src/Components/pages/Home.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react'
 import UserContext from '../../context/UserContext'
+import DashContext from "../../context/DashContext"
 import Scanner from "./Scanner/Scanner"
 import Landing from "./Landing"
-import { Container } from "react-bootstrap"
 import Loading from './Loading'
 
 export default function Home() {
 
   const { userData } = useContext(UserContext)
+  const { dashData } = useContext(DashContext)
 
-  if (userData.isLoading) {
+  if (userData.isLoading || dashData.dashIsLoading ) {
     return (
       <>
       <Loading />
       </>
     )
   }
+
 
   else {
     return (
