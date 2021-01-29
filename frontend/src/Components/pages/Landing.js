4e07ffda4fd92e553337ee825cb443b74faed323
ruben <iamrub3n@gmail.com>
2021-01-29 22:34:09 +0545

    design fixes

--- frontend/src/Components/pages/Landing.js
+++ frontend/src/Components/pages/Landing.js
@@ -5,10 +5,10 @@ import TextLoop from "react-text-loop";
 export default function Landing() {
   return (
     <>
-      <header class="masthead" id="landing-header">
+      <header className="masthead" id="landing-header">
         <Container>
-          <div class="masthead-subheading">Welcome to eReKon</div>
-          <div class="masthead-heading text-uppercase">We Make Recon&nbsp;
+          <div className="masthead-subheading">Welcome to eReKon</div>
+          <div className="masthead-heading text-uppercase">We Make Recon&nbsp;
           <TextLoop children={["Effective", "Easier", "Smooth", "Fast"]} />
           </div>
           <Button variant="info" className="text-uppercase btn-xl" href="https://github.com/rubenkharel/eReKon" > Documentation </Button>
@@ -16,22 +16,22 @@ export default function Landing() {
       </header>
       <section id="features-section" className=" mt-3">
         <Container>
-          <div class="text-center">
-            <h2 class="section-heading text-uppercase">Features</h2>
-            <h3 class="section-subheading text-muted mt-2">Only Features You Will Ever Need</h3>
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">Features</h2>
+            <h3 className="section-subheading text-muted mt-2">Only Features You Will Ever Need</h3>
           </div>
-          <div class="row text-center pt-2 mt-4">
-            <div class="col-md-4">
-              <h4 class="my-3">Scan History</h4>
-              <p class="text-muted">We keep record of all your scan History so that you don't have to.</p>
+          <div className="row text-center pt-2 mt-4">
+            <div className="col-md-4">
+              <h4 className="my-3">Scan History</h4>
+              <p className="text-muted">We keep record of all your scan History so that you don't have to.</p>
             </div>
-            <div class="col-md-4">
-              <h4 class="my-3">Vulnrablity Finder</h4>
-              <p class="text-muted">We Help you find vulnerablity according to the scan results.</p>
+            <div className="col-md-4">
+              <h4 className="my-3">Vulnrablity Finder</h4>
+              <p className="text-muted">We Help you find vulnerablity according to the scan results.</p>
             </div>
-            <div class="col-md-4">
-              <h4 class="my-3">Scan Modes</h4>
-              <p class="text-muted">We have two modes for scan, Fast and Effective.</p>
+            <div className="col-md-4">
+              <h4 className="my-3">Scan Modes</h4>
+              <p className="text-muted">We have two modes for scan, Fast and Effective.</p>
             </div>
           </div>
         </Container>
