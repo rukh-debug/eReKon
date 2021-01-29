c237cd48c46fb11ee070b68dc42a9fe5062dcbce
ruben <iamrub3n@gmail.com>
2021-01-29 22:32:09 +0545

    dashboard fix on start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 import { ToastProvider } from 'react-toast-notifications'
 import ProtectedRoute from "react-protected-route-component"
@@ -17,6 +17,9 @@ import NotFound from './Components/pages/NotFound'
 
 import UserContext from './context/UserContext'
 import ScannedContext from "./context/ScannedContext"
+import DashContext from './context/DashContext'
+import ProgressContext from './context/ProgressContext'
+
 
 export default function App() {
 
@@ -30,6 +33,17 @@ export default function App() {
     data: undefined,
     folderNum: undefined
   });
+
+  const [dashData, setDashData] = useState({
+    data: undefined,
+    dashIsLoading: true
+  })
+
+  const [progressData, setProgressData] = useState({
+    start: false,
+    per: 100,
+    label: "Starting scan..."
+  })
 
   let guardFun = () => {
     if (userData.token) {
@@ -50,7 +64,34 @@ export default function App() {
   }
   // loads everytime the component loads, if dependencies are provided
   // reloads on dependecies changes
+
+  let addToLocal = async (token) => {
+    return new Promise((res) => {
+      res(localStorage.setItem("auth-token", token))
+    });
+  }
+
   useEffect(() => {
+
+    const getDash = async () => {
+      setDashData({ ...dashData, dashIsLoading: true })
+
+      let token = localStorage.getItem("auth-token");
+      let body = { token }
+      await axios.post(`http://localhost:5000/recon/dash`, body,
+        {
+          headers: {
+            "x-auth-token": token
+          }
+        }
+      ).then((res) => {
+        setDashData({ data: res.data.data, dashIsLoading: false })
+      }).catch((e) => {
+        setDashData({ ...dashData, dashIsLoading: false })
+      })
+    }
+
+    setProgressData({ ...progressData })
     const checkLoggedIn = async () => {
       let token = localStorage.getItem("auth-token");
       if (token === null) {
@@ -68,17 +109,21 @@ export default function App() {
 
       // if token was success, get userinfo from that
       if (tokenRes.data) {
+        await addToLocal(token)
         const userRes = await axios.get("http://localhost:5000/users/",
           {
             headers: {
               "x-auth-token": token
             }
           })
+
         setUserData({
           isLoading: false,
           token,
           user: userRes.data
         })
+        await getDash();
+
       }
       else {
         setUserData({
@@ -86,44 +131,51 @@ export default function App() {
           token: undefined,
           user: undefined
         })
+        await getDash();
+
       }
     }
 
     checkLoggedIn();
+
+
+
+
+
   }, [])
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
+      <DashContext.Provider value={{ dashData, setDashData }}>
+        <ProgressContext.Provider value={{ progressData, setProgressData }}>
+          <ScannedContext.Provider value={{ scannedData, setScannedData }}>
 
-      <div>
-        <Header />
-        <ScannedContext.Provider value={{ scannedData, setScannedData }}>
-          <ToastProvider placement="bottom-left">
-            <Switch>
+            <div>
+              <Header />
+              <ToastProvider placement="bottom-left">
+                <Switch>
 
-              <ProtectedRoute path="/dash" redirectRoute="/login" guardFunction={guardFun} component={Dash} />
-              <ProtectedRoute path="/config" redirectRoute="/login" guardFunction={guardFun} component={Config} />
-              <ProtectedRoute path="/setting" redirectRoute="/login" guardFunction={guardFun} component={Settings} />
+                  <ProtectedRoute path="/dash" redirectRoute="/login" guardFunction={guardFun} component={Dash} />
+                  <ProtectedRoute path="/config" redirectRoute="/login" guardFunction={guardFun} component={Config} />
+                  <ProtectedRoute path="/setting" redirectRoute="/login" guardFunction={guardFun} component={Settings} />
 
-              <Route exact path="/" component={Home} />
-              <Route path="/logout" component={Logout} />
+                  <Route exact path="/" component={Home} />
+                  <Route path="/logout" component={Logout} />
 
-              <ProtectedRoute path="/login" redirectRoute="/" guardFunction={guardFunRev} component={Login} />
-              <ProtectedRoute path="/register" redirectRoute="/" guardFunction={guardFunRev} component={Register} />
+                  <ProtectedRoute path="/login" redirectRoute="/" guardFunction={guardFunRev} component={Login} />
+                  <ProtectedRoute path="/register" redirectRoute="/" guardFunction={guardFunRev} component={Register} />
 
-              <Route path="*" component={NotFound} />
+                  <Route path="*" component={NotFound} />
 
-
-
-
-
-
-            </Switch>
-          </ToastProvider>
-        </ScannedContext.Provider>
+                </Switch>
+              </ToastProvider>
 
 
-      </div>
+            </div>
+          </ScannedContext.Provider>
+
+        </ProgressContext.Provider>
+      </DashContext.Provider>
     </UserContext.Provider >
   )
 }
