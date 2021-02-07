16b5b43e91600300fa69d73d52863a98858892f6
Ruben Kharel <iamrub3n@gmail.com>
2021-02-08 02:13:09 +0545

    added getConfig and setConfig

--- backend/routes/reconRouter.js
+++ backend/routes/reconRouter.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 router.post('/fast', auth, async (req, res) => {
   try {
     let { token, url, uuid } = req.body;
-    console.log(token)
     let decoded = jwt.verify(token, process.env.JWT_SECRETS)
 
     // check if the user have ever saved his data or not...
@@ -58,7 +57,7 @@ router.post('/fast', auth, async (req, res) => {
       const userScanned = new ScannedData({
         userRef: decoded.id,
         scanData: scanResponse,
-        forUrl: url
+        forUrl: url,
       })
       const userScannedData = await userScanned.save()
     }
@@ -75,13 +74,15 @@ router.post('/dash', auth, async (req, res) => {
   try {
     let decoded = jwt.verify(token, process.env.JWT_SECRETS)
     const findUserData = await ScannedData.findOne({ userRef: decoded.id })
-    res.status(200).json({ data: findUserData.scanData })
+    res.status(200).json({ data: findUserData?.scanData })
   }
-  catch (e) {
+  catch (e) {  
     res.status(500).json({ error: e.message })
   }
 })
 
+
+//  ((((((( not in use )))))))
 router.post('/progress', async (req, res) => {
   try {
     console.log('hello')
@@ -97,14 +98,10 @@ router.post('/progress', async (req, res) => {
     else {
       folderNum = "0"
     }
-
-
     let raw = fs.readFileSync(`${__dirname}/../static/user/${uuid}/img/${folderNum}/progress.json`)
     let jsonData = JSON.parse(raw)
     console.log(jsonData)
     res.status(200).json(jsonData)
-
-
   }
   catch (e) {
     res.status(500).json({ msg: e.message })
