7fb41fc7ea87e2c1e6fa093860bde771f388d8a6
Ruben Kharel <iamrub3n@gmail.com>
2021-02-08 02:13:30 +0545

    config added while register

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const User = require('../models/userModel')
+const Config = require('../models/configModel')
 const bcrypt = require('bcryptjs')
 const jwt = require("jsonwebtoken")
 const auth = require("../middleware/auth")
 const { v4: uuidv4 } = require('uuid');
 const app = require('../src/controller/app')
+const configModel = require('../models/configModel')
 
 router.post('/register', async (req, res) => {
   try {
@@ -27,67 +29,71 @@ router.post('/register', async (req, res) => {
         .status(401)
         .json({ msg: "Password doesnot match" })
     }
-    const existingUser = await User.findOne({email: email})
+    const existingUser = await User.findOne({ email: email })
     if (existingUser) {
       return res
-      .status(401)
-      .json({msg: "Account with this email already exists"});
+        .status(401)
+        .json({ msg: "Account with this email already exists" });
     }
     if (!displayName) displayName = email;
 
     const salt = await bcrypt.genSalt(10)
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const uuid = uuidv4(); 
+    const uuid = uuidv4();
     app.folderCreator(uuid)
     const newUser = new User({
       email,
-      password: passwordHash, 
+      password: passwordHash,
       displayName,
       uuid,
-      scanMode : 'fast'
     });
 
     const savedUser = await newUser.save();
+    const configData = new Config({
+      userRef: savedUser.id,
+      scanType: "fast"
+    })
+    await configData.save();
     res.status(200).json(savedUser)
 
   }
   catch (e) {
-    res.status(500).json({error: e.message})
+    res.status(500).json({ error: e.message })
   }
 })
 
 
 router.post("/login", async (req, res) => {
-  try{
+  try {
     const { email, password } = req.body;
 
     // validate
-    if(!email || !password){
+    if (!email || !password) {
       return res
-      .status(400)
-      .json({msg: "Not all fields are filled"});
+        .status(400)
+        .json({ msg: "Not all fields are filled" });
     }
 
-    const user = await User.findOne({email: email});
-    if (!user){
+    const user = await User.findOne({ email: email });
+    if (!user) {
       return res
-      .status(400)
-      .json({msg: "No account with this user exist"});
+        .status(400)
+        .json({ msg: "No account with this user exist" });
     }
 
     const isMatched = await bcrypt.compare(password, user.password);
 
-    if (!isMatched){
+    if (!isMatched) {
       return res
-      .status(400)
-      .json({msg: "Invalid credencials."})
+        .status(400)
+        .json({ msg: "Invalid credencials." })
     }
 
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRETS);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETS);
     res.json({
       token,
-      user:{
+      user: {
         id: user._id,
         displayName: user.displayName,
         email: user.email,
@@ -95,37 +101,37 @@ router.post("/login", async (req, res) => {
       }
     })
   }
-  catch(e){
+  catch (e) {
     res.status(500)
-    .json({error: e.message})
+      .json({ error: e.message })
   }
 })
 
 router.delete("/delete", auth, async (req, res) => {
-  try{
+  try {
     const deleteUser = await User.findByIdAndDelete(req.user)
     res.json(deleteUser);
   }
-  catch(e){
-    res.status(500).json({error: err.message});
+  catch (e) {
+    res.status(500).json({ error: err.message });
   }
 })
 
 router.post('/tokenIsValid', async (req, res) => {
-  try{
+  try {
     const token = req.header("x-auth-token");
-    if(!token) return res.json(false);
+    if (!token) return res.json(false);
 
     const verified = jwt.verify(token, process.env.JWT_SECRETS)
-    if(!verified) return res.json(false);
+    if (!verified) return res.json(false);
 
     const user = await User.findById(verified.id);
-    if(!user) return res.json(false);
+    if (!user) return res.json(false);
 
     return res.json(true)
   }
-  catch(err){
-    res.status(500).json({error: err.message})
+  catch (err) {
+    res.status(500).json({ error: err.message })
   }
 })
 
@@ -139,22 +145,36 @@ router.get('/', auth, async (req, res) => {
   });
 })
 
-router.post('/config', auth, async (req, res) => {
-  const { scanMode, bForceSubs  } = req.body;
+router.post('/getConfig', auth, async (req, res) => {
+  try {
+    const { token } = req.body;
+    let decoded = jwt.verify(token, process.env.JWT_SECRETS)
+    const findUserData = await configModel.findOne({ userRef: decoded.id })
+    console.log(findUserData)
+    res.status(200).json({
+      scantype: findUserData.scanType
+    })
+  }
+  catch (err) {
+    res.status(500).json({ msg: err.message })
+  }
+})
+
+router.post('/saveConfig', auth, async (req, res) => {
+  const { token, scanMode } = req.body;
+  let decoded = jwt.verify(token, process.env.JWT_SECRETS)
+
+  const findUserData = await configModel.findOne({ userRef: decoded.id })
 
   // validate...
-  if (!scanMode === 'fast' || !scanMode === 'effective'){
-    res.status(401).json({error: "Invalid scanmode option"})
+  if (!scanMode === 'fast' || !scanMode === 'effective') {
+    res.status(401).json({ error: "Invalid scanmode option" })
   }
 
-  if (scanMode === 'fast'){
-    res.status(200).json({msg: "Config Saved"})
+  if (scanMode === 'fast') {
+    res.status(200).json({ msg: "Config Saved" })
   }
 
-  if (scanMode === "effective" && typeOf(bForceSubs) === 'string' ){
-
-  }
-  
 })
 
 
